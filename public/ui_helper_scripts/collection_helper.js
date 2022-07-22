97c88a928499ab30932f2aa40a249562bb9bc85a
Nishanth Dass <dassn@flip2.engr.oregonstate.edu>
2022-07-22 16:41:16 -0700

    7:41PM EST 7_22_22 Nish

--- public/ui_helper_scripts/collection_helper.js
+++ public/ui_helper_scripts/collection_helper.js
@@ -1,4 +1,8 @@
-$(document).on("click", ".open-AddBookDialog", function () {
+// Helper function to move data from front end tables to bootstrap modal for Update/Edit
+// https://stackoverflow.com/questions/10626885/passing-data-to-a-bootstrap-modal
+
+$(document).on("click", ".open-editCol", function () {
     var myBookId = $(this).data('id');
-    $(".modal-body #bookId").val(myBookId);
+    $(".modal-body #coleditId").val(myBookId.id);
+    $(".modal-body #coleditName").val(myBookId.name);
 });
